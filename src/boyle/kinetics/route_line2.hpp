BoyleDevTeam/boyle

[Base] refactor project

src/boyle/kinetics/route_line2.hpp
@@ -1,0 +1,142 @@
+/**
+ * @file route_line2.hpp
+ * @author Houchen Li (houchen_li@hotmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-06
+ *
+ * @copyright Copyright (c) 2023 Boyle Development Team
+ *            All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include <array>
+#include <concepts>
+#include <utility>
+#include <vector>
+
+#include "boost/serialization/access.hpp"
+
+#include "boyle/math/curves/piecewise_curves/piecewise_quintic_curve2.hpp"
+#include "boyle/math/utils.hpp"
+#include "boyle/math/vec2.hpp"
+
+namespace boyle::kinetics {
+
+template <std::floating_point T>
+class [[nodiscard]] RouteLine2 final {
+    friend class boost::serialization::access;
+
+  public:
+    using BoundaryMode = typename ::boyle::math::PiecewiseQuinticCurve2<T>::BoundaryMode;
+
+    RouteLine2() noexcept = default;
+    RouteLine2(const RouteLine2& other) noexcept = default;
+    auto operator=(const RouteLine2& other) noexcept -> RouteLine2& = default;
+    RouteLine2(RouteLine2&& other) noexcept = default;
+    auto operator=(RouteLine2&& other) noexcept -> RouteLine2& = default;
+    ~RouteLine2() noexcept = default;
+
+    [[using gnu: always_inline]]
+    explicit RouteLine2(std::vector<::boyle::math::Vec2<T>> anchor_points, T s0 = 0.0)
+        : RouteLine2{
+              std::move(anchor_points),
+              std::array<BoundaryMode, 2>{
+                  BoundaryMode{2, ::boyle::math::Vec2<T>{0.0, 0.0}},
+                  BoundaryMode{4, ::boyle::math::Vec2<T>{0.0, 0.0}}
+              },
+              std::array<BoundaryMode, 2>{
+                  BoundaryMode{2, ::boyle::math::Vec2<T>{0.0, 0.0}},
+                  BoundaryMode{4, ::boyle::math::Vec2<T>{0.0, 0.0}}
+              },
+              s0
+          } {}
+
+    [[using gnu: always_inline]]
+    explicit RouteLine2(
+        std::vector<::boyle::math::Vec2<T>> anchor_points, std::array<BoundaryMode, 2> b0,
+        std::array<BoundaryMode, 2> bf, T s0 = 0.0
+    )
+        : m_curve{anchor_points, b0, bf, s0} {}
+
+    [[using gnu: pure, always_inline]]
+    auto operator()(T s) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto operator()(T s, T l) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve(s, l);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto operator()(::boyle::math::SlDuplet<T> sl) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve(sl);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto inverse(::boyle::math::Vec2<T> point) const noexcept -> ::boyle::math::SlDuplet<T> {
+        return m_curve.inverse(point);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto inverse(::boyle::math::Vec2<T> point, T start_s, T end_s) const noexcept
+        -> ::boyle::math::SlDuplet<T> {
+        return m_curve.inverse(point, start_s, end_s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto tangent(T s) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve.tangent(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto orthnormal(T s) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve.orthonormal(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto curvature(T s) -> T {
+        return m_curve.curvature(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto minS() const noexcept -> T {
+        return m_curve.minS();
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto maxS() const noexcept -> T {
+        return m_curve.maxS();
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto arcLengths() const noexcept -> const std::vector<T>& {
+        return m_curve.arcLengths();
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto anchorPoints() const noexcept -> const std::vector<::boyle::math::Vec2<T>>& {
+        return m_curve.anchorPoints();
+    }
+
+  private:
+    [[using gnu: always_inline]]
+    explicit RouteLine2(::boyle::math::PiecewiseQuinticCurve2<T> curve) noexcept
+        : m_curve{std::move(curve)} {}
+
+    [[using gnu: always_inline]]
+    auto serialize(auto& archive) noexcept -> void {
+        archive & m_curve;
+        return;
+    }
+
+    ::boyle::math::PiecewiseQuinticCurve2<T> m_curve{};
+};
+
+using RouteLine2f = RouteLine2<float>;
+using RouteLine2d = RouteLine2<double>;
+
+} // namespace boyle::kinetics

tests/boyle/math/vec3_test.cpp
@@ -1,0 +1,172 @@
+/**
+ * @file vec3_test.cpp
+ * @author Houchen Li (houchen_li@hotmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-07-17
+ *
+ * @copyright Copyright (c) 2023 Boyle Development Team
+ *            All rights reserved.
+ *
+ */
+
+#include "boyle/math/vec3.hpp"
+
+#include <cmath>
+#include <format>
+#include <sstream>
+
+#include "boost/archive/binary_iarchive.hpp"
+#include "boost/archive/binary_oarchive.hpp"
+#include "fmt/format.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+namespace boyle::math {
+
+TEST_CASE("Constructor") {
+    Vec3d a(0.0, 0.0, 0.0);
+    CHECK_EQ(a.x, 0.0);
+    CHECK_EQ(a.y, 0.0);
+    CHECK_EQ(a.z, 0.0);
+    CHECK_EQ(sizeof(a.x), 8);
+    CHECK_EQ(sizeof(a.y), 8);
+    CHECK_EQ(sizeof(a.z), 8);
+
+    Vec3d b(a);
+    CHECK_EQ(b.x, a.x);
+    CHECK_EQ(b.y, a.y);
+    CHECK_EQ(b.z, a.z);
+
+    Vec3f c(a);
+    CHECK_EQ(c.x, 0.0f);
+    CHECK_EQ(c.y, 0.0f);
+    CHECK_EQ(c.z, 0.0f);
+    CHECK_EQ(sizeof(c.x), 4);
+    CHECK_EQ(sizeof(c.y), 4);
+    CHECK_EQ(sizeof(c.z), 4);
+
+    a.x = 3.0;
+    a.y = 4.0;
+    a.z = 5.0;
+    c = b = a;
+    CHECK_EQ(c.x, 3.0);
+    CHECK_EQ(c.y, 4.0);
+    CHECK_EQ(c.z, 5.0);
+}
+
+TEST_CASE("Basic") {
+    const Vec3d a(1.0, std::sqrt(3.0), std::sqrt(5.0));
+    CHECK_EQ(a.norm(), 3.0);
+    CHECK_EQ(std::hypot(a), 3.0);
+
+    const Vec3d c = normalize(a);
+    CHECK_EQ(c.x, 1.0 / 3.0);
+    CHECK_EQ(c.y, std::sqrt(3.0) / 3.0);
+    CHECK_EQ(c.z, std::sqrt(5.0) / 3.0);
+}
+
+TEST_CASE("Arithmetic") {
+    const double sqrt_3 = std::sqrt(3.0);
+    const Vec3d a(1.0, 0.0, 1.0);
+    const Vec3d b(0.5, sqrt_3 * 0.5, -1.0);
+
+    Vec3d c = a + b;
+    CHECK_EQ(c.x, 1.5);
+    CHECK_EQ(c.y, sqrt_3 * 0.5);
+    CHECK_EQ(c.z, 0.0);
+    CHECK_EQ(c.norm(), sqrt_3);
+
+    Vec3d d = c - b;
+    CHECK_EQ(d.x, a.x);
+    CHECK_EQ(d.y, a.y);
+    CHECK_EQ(d.z, a.z);
+
+    c -= b;
+    CHECK_EQ(c.x, a.x);
+    CHECK_EQ(c.y, a.y);
+    CHECK_EQ(c.z, a.z);
+
+    c += b;
+    CHECK_EQ(c.x, 1.5);
+    CHECK_EQ(c.y, sqrt_3 * 0.5);
+    CHECK_EQ(c.z, 0.0);
+
+    c = a * 0.5;
+    CHECK_EQ(c.x, a.x * 0.5);
+    CHECK_EQ(c.y, a.y * 0.5);
+    CHECK_EQ(c.z, a.z * 0.5);
+
+    c = 0.5 * a;
+    CHECK_EQ(c.x, a.x * 0.5);
+    CHECK_EQ(c.y, a.y * 0.5);
+    CHECK_EQ(c.z, a.z * 0.5);
+
+    c = a;
+    c *= 0.5;
+    CHECK_EQ(c.x, a.x * 0.5);
+    CHECK_EQ(c.y, a.y * 0.5);
+    CHECK_EQ(c.z, a.z * 0.5);
+
+    c = a / 2.0;
+    CHECK_EQ(c.x, a.x / 2.0);
+    CHECK_EQ(c.y, a.y / 2.0);
+    CHECK_EQ(c.z, a.z / 2.0);
+
+    c = a;
+    c /= 2.0;
+    CHECK_EQ(c.x, a.x / 2.0);
+    CHECK_EQ(c.y, a.y / 2.0);
+    CHECK_EQ(c.z, a.z / 2.0);
+
+    const double a_dot_b = a.dot(b);
+    CHECK_EQ(a_dot_b, -0.5);
+
+    const Vec3d b_cross_c = b.cross(c);
+    CHECK_EQ(b_cross_c.x, sqrt_3 * 0.25);
+    CHECK_EQ(b_cross_c.y, -0.75);
+    CHECK_EQ(b_cross_c.z, -sqrt_3 * 0.25);
+}
+
+TEST_CASE("OstreamOperator") {
+    const Vec3d a(1274.12, 4454.23, 1289.24);
+
+    std::ostringstream oss;
+    oss << a;
+
+    CHECK_EQ(oss.str(), "(x: 1274.12, y: 4454.23, z: 1289.24)");
+}
+
+TEST_CASE("Format") {
+    const Vec3d a(1274.12, 4454.23, -23.5745);
+
+    CHECK_EQ(std::format("{}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(std::format("{0}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(std::format("{0:.2}", a), "(x: 1274.12, y: 4454.23, z: -23.57)");
+    CHECK_EQ(std::format("{0:12}", a), "(x:  1274.120000, y:  4454.230000, z:   -23.574500)");
+    CHECK_EQ(std::format("{0:12.2}", a), "(x:      1274.12, y:      4454.23, z:       -23.57)");
+
+    CHECK_EQ(fmt::format("{}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(fmt::format("{0}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(fmt::format("{0:.2}", a), "(x: 1274.12, y: 4454.23, z: -23.57)");
+    CHECK_EQ(fmt::format("{0:12}", a), "(x:  1274.120000, y:  4454.230000, z:   -23.574500)");
+    CHECK_EQ(fmt::format("{0:12.2}", a), "(x:      1274.12, y:      4454.23, z:       -23.57)");
+}
+
+TEST_CASE("Serialization") {
+    const Vec3d a(1274.12, 4454.23, 1289.24);
+    std::ostringstream oss;
+    boost::archive::binary_oarchive oa(oss);
+    oa << a;
+
+    Vec3d b;
+    std::istringstream iss(oss.str());
+    boost::archive::binary_iarchive ia(iss);
+    ia >> b;
+
+    CHECK_EQ(a.x, b.x);
+    CHECK_EQ(a.y, b.y);
+}
+
+} // namespace boyle::math

Tiny-PnC/tiny_pnc

src/boyle/kinetics/route_line2.hpp
@@ -1,0 +1,138 @@
+/**
+ * @file route_line2.hpp
+ * @author Houchen Li (houchen_li@hotmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-06
+ *
+ * @copyright Copyright (c) 2023 Boyle Development Team
+ *            All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include <array>
+#include <concepts>
+#include <utility>
+#include <vector>
+
+#include "boost/serialization/access.hpp"
+
+#include "boyle/math/curves/piecewise_quintic_curve.hpp"
+#include "boyle/math/vec2.hpp"
+
+namespace boyle::kinetics {
+
+template <std::floating_point T>
+class [[nodiscard]] RouteLine2 final {
+    friend class boost::serialization::access;
+
+  public:
+    using BoundaryMode =
+        typename ::boyle::math::PiecewiseQuinticCurve<::boyle::math::Vec2<T>>::BoundaryMode;
+
+    RouteLine2() noexcept = default;
+    RouteLine2(const RouteLine2& other) noexcept = default;
+    auto operator=(const RouteLine2& other) noexcept -> RouteLine2& = default;
+    RouteLine2(RouteLine2&& other) noexcept = default;
+    auto operator=(RouteLine2&& other) noexcept -> RouteLine2& = default;
+    ~RouteLine2() noexcept = default;
+
+    [[using gnu: always_inline]]
+    explicit RouteLine2(std::vector<::boyle::math::Vec2<T>> anchor_points, T s0 = 0.0)
+        : RouteLine2{
+              std::move(anchor_points),
+              std::array<BoundaryMode, 2>{
+                  BoundaryMode{2, ::boyle::math::Vec2<T>{0.0, 0.0}},
+                  BoundaryMode{4, ::boyle::math::Vec2<T>{0.0, 0.0}}
+              },
+              std::array<BoundaryMode, 2>{
+                  BoundaryMode{2, ::boyle::math::Vec2<T>{0.0, 0.0}},
+                  BoundaryMode{4, ::boyle::math::Vec2<T>{0.0, 0.0}}
+              },
+              s0
+          } {}
+
+    [[using gnu: always_inline]]
+    explicit RouteLine2(
+        std::vector<::boyle::math::Vec2<T>> anchor_points, std::array<BoundaryMode, 2> b0,
+        std::array<BoundaryMode, 2> bf, T s0 = 0.0
+    )
+        : m_curve{anchor_points, b0, bf, s0} {}
+
+    [[using gnu: pure, always_inline]]
+    auto operator()(T s) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto operator()(T s, T l) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve(s, l);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto operator()(::boyle::math::SlDuplet<T> sl) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve(sl);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto inverse(::boyle::math::Vec2<T> point) const noexcept -> ::boyle::math::SlDuplet<T> {
+        return m_curve.inverse(point);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto inverse(::boyle::math::Vec2<T> point, T start_s, T end_s) const noexcept
+        -> ::boyle::math::SlDuplet<T> {
+        return m_curve.inverse(point, start_s, end_s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto tangent(T s) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve.tangent(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto normal(T s) const noexcept -> ::boyle::math::Vec2<T> {
+        return m_curve.normal(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto curvature(T s) -> T {
+        return m_curve.curvature(s);
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto minS() const noexcept -> T {
+        return m_curve.minS();
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto maxS() const noexcept -> T {
+        return m_curve.maxS();
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto arcLengths() const noexcept -> const std::vector<T>& {
+        return m_curve.arcLengths();
+    }
+
+    [[using gnu: pure, always_inline]]
+    auto anchorPoints() const noexcept -> const std::vector<::boyle::math::Vec2<T>>& {
+        return m_curve.anchorPoints();
+    }
+
+  private:
+    [[using gnu: always_inline]]
+    auto serialize(auto& archive) noexcept -> void {
+        archive & m_curve;
+        return;
+    }
+
+    ::boyle::math::PiecewiseQuinticCurve<::boyle::math::Vec2<T>> m_curve{};
+};
+
+using RouteLine2f = RouteLine2<float>;
+using RouteLine2d = RouteLine2<double>;
+
+} // namespace boyle::kinetics

tests/boyle/math/vec3_test.cpp
@@ -1,0 +1,173 @@
+/**
+ * @file vec3_test.cpp
+ * @author Houchen Li (houchen_li@hotmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-07-17
+ *
+ * @copyright Copyright (c) 2023 Boyle Development Team
+ *            All rights reserved.
+ *
+ */
+
+#include "boyle/math/vec3.hpp"
+
+#include <cmath>
+#include <format>
+#include <sstream>
+
+#include "boost/archive/binary_iarchive.hpp"
+#include "boost/archive/binary_oarchive.hpp"
+#include "fmt/format.h"
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+namespace boyle::math {
+
+TEST_CASE("Constructor") {
+    Vec3d a(0.0, 0.0, 0.0);
+    CHECK_EQ(a.x, 0.0);
+    CHECK_EQ(a.y, 0.0);
+    CHECK_EQ(a.z, 0.0);
+    CHECK_EQ(sizeof(a.x), 8);
+    CHECK_EQ(sizeof(a.y), 8);
+    CHECK_EQ(sizeof(a.z), 8);
+
+    Vec3d b(a);
+    CHECK_EQ(b.x, a.x);
+    CHECK_EQ(b.y, a.y);
+    CHECK_EQ(b.z, a.z);
+
+    Vec3f c(a);
+    CHECK_EQ(c.x, 0.0F);
+    CHECK_EQ(c.y, 0.0F);
+    CHECK_EQ(c.z, 0.0F);
+    CHECK_EQ(sizeof(c.x), 4);
+    CHECK_EQ(sizeof(c.y), 4);
+    CHECK_EQ(sizeof(c.z), 4);
+
+    a.x = 3.0;
+    a.y = 4.0;
+    a.z = 5.0;
+    c = b = a;
+    CHECK_EQ(c.x, 3.0);
+    CHECK_EQ(c.y, 4.0);
+    CHECK_EQ(c.z, 5.0);
+}
+
+TEST_CASE("Basic") {
+    const Vec3d a(1.0, std::sqrt(3.0), std::sqrt(5.0));
+    CHECK_EQ(a.euclidean(), 3.0);
+    CHECK_EQ(std::hypot(a), 3.0);
+
+    const Vec3d c = a.normalized();
+    CHECK_EQ(c.x, 1.0 / 3.0);
+    CHECK_EQ(c.y, std::sqrt(3.0) / 3.0);
+    CHECK_EQ(c.z, std::sqrt(5.0) / 3.0);
+}
+
+TEST_CASE("Arithmetic") {
+    const double sqrt_3 = std::sqrt(3.0);
+    const Vec3d a(1.0, 0.0, 1.0);
+    const Vec3d b(0.5, sqrt_3 * 0.5, -1.0);
+
+    Vec3d c = a + b;
+    CHECK_EQ(c.x, 1.5);
+    CHECK_EQ(c.y, sqrt_3 * 0.5);
+    CHECK_EQ(c.z, 0.0);
+    CHECK_EQ(c.euclidean(), sqrt_3);
+
+    Vec3d d = c - b;
+    CHECK_EQ(d.x, a.x);
+    CHECK_EQ(d.y, a.y);
+    CHECK_EQ(d.z, a.z);
+
+    c -= b;
+    CHECK_EQ(c.x, a.x);
+    CHECK_EQ(c.y, a.y);
+    CHECK_EQ(c.z, a.z);
+
+    c += b;
+    CHECK_EQ(c.x, 1.5);
+    CHECK_EQ(c.y, sqrt_3 * 0.5);
+    CHECK_EQ(c.z, 0.0);
+
+    c = a * 0.5;
+    CHECK_EQ(c.x, a.x * 0.5);
+    CHECK_EQ(c.y, a.y * 0.5);
+    CHECK_EQ(c.z, a.z * 0.5);
+
+    c = 0.5 * a;
+    CHECK_EQ(c.x, a.x * 0.5);
+    CHECK_EQ(c.y, a.y * 0.5);
+    CHECK_EQ(c.z, a.z * 0.5);
+
+    c = a;
+    c *= 0.5;
+    CHECK_EQ(c.x, a.x * 0.5);
+    CHECK_EQ(c.y, a.y * 0.5);
+    CHECK_EQ(c.z, a.z * 0.5);
+
+    c = a / 2.0;
+    CHECK_EQ(c.x, a.x / 2.0);
+    CHECK_EQ(c.y, a.y / 2.0);
+    CHECK_EQ(c.z, a.z / 2.0);
+
+    c = a;
+    c /= 2.0;
+    CHECK_EQ(c.x, a.x / 2.0);
+    CHECK_EQ(c.y, a.y / 2.0);
+    CHECK_EQ(c.z, a.z / 2.0);
+
+    const double a_dot_b = a.dot(b);
+    CHECK_EQ(a_dot_b, -0.5);
+
+    const Vec3d b_cross_c = b.cross(c);
+    CHECK_EQ(b_cross_c.x, sqrt_3 * 0.25);
+    CHECK_EQ(b_cross_c.y, -0.75);
+    CHECK_EQ(b_cross_c.z, -sqrt_3 * 0.25);
+}
+
+TEST_CASE("OstreamOperator") {
+    const Vec3d a(1274.12, 4454.23, 1289.24);
+
+    std::ostringstream oss;
+    oss << a;
+
+    CHECK_EQ(oss.str(), "(x: 1274.12, y: 4454.23, z: 1289.24)");
+}
+
+TEST_CASE("Format") {
+    constexpr Vec3d a(1274.12, 4454.23, -23.5745);
+
+    CHECK_EQ(std::format("{}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(std::format("{0}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(std::format("{0:.2}", a), "(x: 1274.12, y: 4454.23, z: -23.57)");
+    CHECK_EQ(std::format("{0:12}", a), "(x:  1274.120000, y:  4454.230000, z:   -23.574500)");
+    CHECK_EQ(std::format("{0:12.2}", a), "(x:      1274.12, y:      4454.23, z:       -23.57)");
+
+    CHECK_EQ(fmt::format("{}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(fmt::format("{0}", a), "(x: 1274.12, y: 4454.23, z: -23.5745)");
+    CHECK_EQ(fmt::format("{0:.2}", a), "(x: 1274.12, y: 4454.23, z: -23.57)");
+    CHECK_EQ(fmt::format("{0:12}", a), "(x:  1274.120000, y:  4454.230000, z:   -23.574500)");
+    CHECK_EQ(fmt::format("{0:12.2}", a), "(x:      1274.12, y:      4454.23, z:       -23.57)");
+}
+
+TEST_CASE("Serialization") {
+    constexpr Vec3d a(1274.12, 4454.23, 1289.24);
+
+    std::ostringstream oss;
+    boost::archive::binary_oarchive oa(oss);
+    oa << a;
+
+    Vec3d b;
+    std::istringstream iss(oss.str());
+    boost::archive::binary_iarchive ia(iss);
+    ia >> b;
+
+    CHECK_EQ(a.x, b.x);
+    CHECK_EQ(a.y, b.y);
+}
+
+} // namespace boyle::math